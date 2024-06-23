3d53aeea14f9a1686b30f7c5b7319475ff5dfacb
tiakashyap <tia.kashyap.2005@gmail.com>
2024-06-23 15:02:09 -0500

    added home page

--- responsive-website-1/app.js
+++ responsive-website-1/app.js
@@ -1,9 +1,11 @@
 //create variables using ids
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
+const mainButton = document.querySelector('.main-button');
 
 //when menu is clicked, toggle is-active and active classes on and off
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+    mainButton.classList.toggle('hidden');
 });
