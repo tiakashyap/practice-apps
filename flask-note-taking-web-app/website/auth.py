9fab079f80f1a4b2e27bd9c64e9d6a0c7976c64e
tiakashyap <tia.kashyap.2005@gmail.com>
2024-06-28 15:45:26 -0500

    html templates, handling post requests, sqlalchemy setup

---
+++ flask-note-taking-web-app/website/auth.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, request, flash
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    return render_template("login.html", text="Testing", user="Tim")
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif password1 < 7:
+            flash('Password myst be at least 7 characters', category='error')
+        else:
+            #add user to database
+            flash('Account created!', category='success')
+
+    return render_template("sign_up.html")
--- flask-note-taking-web-app/website/views.py
+++ flask-note-taking-web-app/website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 #run when we are at home
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
